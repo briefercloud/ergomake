dd8d95392d9b254a16e00e9fc36ed4b2c34c7f26
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2023-07-09 20:37:44 -0700

    add repo add button

--- front/src/pages/Projects.tsx
+++ front/src/pages/Projects.tsx
@@ -1,7 +1,8 @@
+import { PlusIcon } from '@heroicons/react/20/solid'
 import { useMemo, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
-import Layout from '../components/Layout'
+import Layout, { installationUrl } from '../components/Layout'
 import RepositoryList from '../components/RepositoryList'
 import { orElse } from '../hooks/useHTTPRequest'
 import { useOwners } from '../hooks/useOwners'
@@ -40,6 +41,20 @@ const Projects = ({ profile }: Props) => {
 
   return (
     <Layout profile={profile} pages={pages}>
+      <div className="bg-white border-b border-gray-200 flex flex-col items-start justify-between gap-x-8 gap-y-4 bg-white px-4 py-4 sm:flex-row sm:items-center sm:px-6 lg:px-8  ">
+        <h1 className="flex text-2xl tracking-tight font-semibold text-gray-800 sm:text-4xl h-20 items-center">
+          Repositories
+        </h1>
+
+        <a
+          href={installationUrl}
+          className="inline-flex items-center rounded-md bg-primary-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+        >
+          <PlusIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
+          Add repository
+        </a>
+      </div>
+
       <RepositoryList repos={repos} />
     </Layout>
   )

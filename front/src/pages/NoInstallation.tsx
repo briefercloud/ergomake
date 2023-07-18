a9997520f6f5b0d90f1e3135dc20337860740865
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2023-07-18 08:37:21 -0700

    add modal on empty state for orgs

--- front/src/pages/NoInstallation.tsx
+++ front/src/pages/NoInstallation.tsx
@@ -42,13 +42,13 @@ const NoInstallation = ({ profile }: NoInstallationProps) => {
           </p>
 
           <div className="pt-6">
-            <a
-              href={installationUrl}
+            <button
+              onClick={() => setIsPermissionsModalOpen(true)}
               className="inline-flex items-center rounded-md bg-primary-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
             >
               <PlusIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
               Add organization
-            </a>
+            </button>
           </div>
         </div>
       </div>
